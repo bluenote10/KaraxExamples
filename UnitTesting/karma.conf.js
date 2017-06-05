8bda6d718494ccafd9076ad06020186af67f8bea
Fabian Keller <github.100.fkeller@spamgourmet.com>
2017-06-05 10:44:52 +0200

    simplified karma integration

--- UnitTesting/karma.conf.js
+++ UnitTesting/karma.conf.js
@@ -11,13 +11,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // the main test file is served by Karma
-      'nimcache/test.js',
-      {
-        pattern: "**/*.nim",
-        watched: true,
-        served: false,
-        included: false
-      }
+      'nimcache/test.js'
     ],
 
     // list of files to exclude
@@ -27,11 +21,6 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.nim': ['nim']
-    },
-
-    nimPreprocessor: {
-      cmd: 'nim js --colors:on test.nim'
     },
 
     // test results reporter to use
